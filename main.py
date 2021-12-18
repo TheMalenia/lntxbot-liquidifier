6ad062f10ef80b601679f7cb487549d697cfed5d
Powder <82274616+JinxPowder@users.noreply.github.com>
2021-12-18 23:17:01 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from telethon.sync import TelegramClient, events
+import config
+import time
+import re
+from random import randrange
+
+api_id = config.API_ID
+api_hash = config.API_HASH
+
+INVOICE = "/paynow invoice"
+
+with TelegramClient("session-name" , api_id, api_hash) as client:
+   print("Started.")
+   @client.on(events.NewMessage(from_users="lntxbot"))
+   async def handler(event):
+        try:
+            if(event.chat_id == 738657014):
+                text = (event.message.message)
+                regex = "You've lost"
+                match = re.search(regex , text)
+                if(match):
+                    await client.send_message(entity=738657014 , message= INVOICE )
+        except Exception as e:
+            pass
+   client.run_until_disconnected()
